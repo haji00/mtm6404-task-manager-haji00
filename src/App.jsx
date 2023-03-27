f5614958b617dae00f5dd71b76a9ea26ad619160
Sepidah Hajiyousefi <haji0038@algonquinlive.com>
2023-03-27 19:50:45 -0400

    Completes iteration 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import Button from "./Button/Button";
 import { useState } from "react";
 import { NavBar } from "./NavBar/NavBar";
 import { tasks } from "./assets/tasks";
@@ -9,19 +8,121 @@ import Footer from "./Footer/Footer";
 
 function App() {
   const [user, setUser] = useState();
+  const [taskList, setTaskList] = useState(tasks);
+  const [newItemTask, setNewItemTask] = useState("");
+  const [newItemPriority, setNewItemPriority] = useState("High");
+  const [showCompleted, setShowCompleted] = useState(true);
+
+  const addItemToList = () => {
+    setTaskList([
+      ...taskList,
+      {
+        id: taskList.length,
+        taskItem: newItemTask,
+        priority: newItemPriority,
+        status: "Incomplete",
+      },
+    ]);
+  };
+
+  const figureoutStatus = (id) => {
+    const itemStatus = taskList[id].status;
+    if (itemStatus === "Completed") {
+      return "Incomplete";
+    }
+    return "Completed";
+  };
+
+  const saveMyChange = () => {
+    localStorage.setItem("list", JSON.stringify(taskList));
+  };
+
+  const handleChange = (id) => {
+    const [...taskListCopy] = taskList;
+    console.log("Id", id);
+    console.log("status", taskList[id].status);
+    taskListCopy[id].status = figureoutStatus(id);
+    setTaskList(taskListCopy);
+    saveMyChange();
+  };
+
+  const handleDelete = (id) => {
+    const taskListCopy = taskList.filter((task) => task.id !== id);
+    console.log("Id", id);
+    setTaskList(taskListCopy);
+    saveMyChange();
+  };
 
   return (
     <div className="App">
       <NavBar />
-      <div className="card">
-        <Button label="Counter disabled " enabled={false} />
-        <Button initialCount={10} label="disabled" enable={false} />
-        <Button initialCount={0} label="Add New Task " enabled={true} />
+      <div className="form">
+        <button
+          className="add-btn"
+          onClick={addItemToList}
+          disabled={!newItemTask}
+        >
+          Add Item
+        </button>
+        <input
+          value={newItemTask}
+          onChange={(event) => {
+            setNewItemTask(event.target.value);
+          }}
+        />
+        <select
+          onChange={(ev) => {
+            setNewItemPriority(ev.target.value);
+          }}
+        >
+          <option value="High">High</option>
+          <option value="Medium">Medium</option>
+          <option value="Low">Low</option>
+        </select>
       </div>
+
+      <div className="show-completed">
+        <label>Show All Tasks</label>
+        <input
+          id="showCompleted"
+          type="checkbox"
+          checked={showCompleted}
+          onChange={() => setShowCompleted(!showCompleted)}
+        />
+      </div>
+
       <ul className="task-list">
-        {tasks.map((task) =>
-          task.completed ? null : <ListItem task={task} />
-        )}
+        {taskList
+          .sort((a, b) => {
+            if (a.priority === "High") {
+              return -1;
+            } else if (a.priority === "Low") {
+              return 1;
+            } else if (a.priority === "Medium") {
+              return 0;
+            }
+          })
+          .map((task) =>
+            task.status !== "Completed" ? (
+              <ListItem
+                key={task.id}
+                task={task}
+                isCompleted={false}
+                onToggleStatus={() => handleChange(task.id - 1)}
+                onDeleteTask={() => handleDelete(task.id)}
+              />
+            ) : (
+              showCompleted && (
+                <ListItem
+                  key={task.id}
+                  task={task}
+                  isCompleted={true}
+                  onToggleStatus={() => handleChange(task.id - 1)}
+                  onDeleteTask={() => handleDelete(task.id)}
+                />
+              )
+            )
+          )}
       </ul>
       <Footer />
     </div>
--- src/ListItem/ListItem.jsx
+++ src/ListItem/ListItem.jsx
@@ -1,12 +1,29 @@
 import "./listItem.css";
 
-const ListItem = ({ task }) => {
+const ListItem = ({ task, onToggleStatus, onDeleteTask }) => {
+  const handleStatusToggle = () => {
+    onToggleStatus(task.id);
+  };
+
+  const handleDelete = () => {
+    onDeleteTask(task.id);
+  };
+
   return (
-    <li className="list">
-      <p className="list-id">{task.id}</p>
-      <p className="list-item">{task.taskItem}</p>
-      <p className="list-type">{task.itemType}</p>
-    </li>
+    <div className="container">
+      <li className="list">
+        <p className="list-item">{task.taskItem}</p>
+        <p className="list-type">{task.priority}</p>
+      </li>
+      <div className="buttons">
+        <button className="status-btn" onClick={handleStatusToggle}>
+          {task.status}
+        </button>
+        <button className="delete-btn" onClick={handleDelete}>
+          Delete
+        </button>
+      </div>
+    </div>
   );
 };
 
