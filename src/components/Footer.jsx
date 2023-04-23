028067a10759a49db19e1f412571cf4d2bbfa2df
Sepidah Hajiyousefi <haji0038@algonquinlive.com>
2023-04-23 18:05:19 -0400

    Completes the assignment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 import "../App.css";
 
 const Footer = () => {
   return (
-    <footer style={{position: "fixed", bottom: "0", left: "0", right: "0"}}><p style={{textAlign: "center", fontSize: "15px", paddingTop: "80px"}}>&copy; 2023 SEPIDAH HAJIYOUSEFI | All rights reserved</p></footer>
+    <footer
+      style={{ position: "fixed", bottom: "-10px", left: "0", right: "0" }}
+    >
+      <p style={{ textAlign: "center", fontSize: "15px", paddingTop: "10px" }}>
+        &copy; 2023 SEPIDAH HAJIYOUSEFI | All rights reserved
+      </p>
+    </footer>
   );
 };
 
